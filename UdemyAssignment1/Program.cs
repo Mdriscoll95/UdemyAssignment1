29f468f51dd643778192dd89d1446c1a36924865
Michael Driscoll <michael.driscoll951@gmail.com>
2023-07-12 13:20:02 -0700

    UdemyAssignment1

---
+++ UdemyAssignment1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace UdemyAssignment1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string stringForFloat = "0.85"; // datatype should be float
+        string stringForInt = "12345"; // datatype should be int
+        float f = float.Parse(stringForFloat);
+        int i = Int32.Parse(stringForInt);
+
+        System.Console.WriteLine("Int = {0}, Float = {1}", i, f);
+        Console.Read();
+    }
+}
+
